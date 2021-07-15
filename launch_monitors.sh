639350bf05e470d9df772f8cceee3dc39e00e003
Pascal Kuschkowitz <pascal2003.k@gmail.com>
2021-07-15 20:48:38 +0200

    Fix issues after update

--- launch_monitors.sh
+++ launch_monitors.sh
@@ -4,12 +4,12 @@
 # You will need to ajust the names of each monitor
 
 myc=$(xrandr --query | grep 'HDMI-1-0')
-if [[ $myc == "HDMI-1-0 connect primary"* ]]
+if [[ $myc == "HDMI-1-0 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 510mm x 290mm" ]]
     then
-        bspc monitor HDMI-1-0 -d VS II III GK V
-        bspc monitor eDP1 -d VI VII Sp
+        bspc monitor HDMI-1-0 -d VS II III GK V 
+        bspc monitor eDP1 -d VI VII Sp 
     else 
-        bspc monitor eDP1 -d I II III IV
+        bspc monitor eDP1 -d I II III IV IIIII 
 fi
 
     
